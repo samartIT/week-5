66bdbf058857f251d202755f74e208668a644667
FAHKRAM <59070002@it.kmitl.ac.th>
2019-02-16 00:35:46 +0700

    Part 8 : Multiple Cameras Script

---
+++ week-5-unity-lab/Assets/_59070002/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Camera cameraTopView;
+    public Camera cameraFPSView;
+    void Start()
+    {
+        cameraTopView = Camera.main;
+        cameraTopView.enabled = true;
+        cameraFPSView.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C)) {
+            cameraTopView.enabled = !cameraTopView.enabled;
+            cameraFPSView.enabled = !cameraFPSView.enabled;
+        }
+    }
+}
