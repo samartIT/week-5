12f459ba4f3e7ef23c53e9261b930429f34dfc5b
Munawang <59070036@it.kmitl.ac.th>
2019-02-17 22:20:37 +0700

    Add MouseLook + Horizantal

--- week-5-unity-lab/Assets/_59070036/Scripts/MouseLook.cs
+++ week-5-unity-lab/Assets/_59070036/Scripts/MouseLook.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+
+    }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityHor = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,10 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(axes == RotationAxes.MouseX){
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+        } else if (axes == RotationAxes.MouseY){
+        } else{
+        }
     }
 }
