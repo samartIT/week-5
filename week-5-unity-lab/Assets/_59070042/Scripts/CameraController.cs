2836198eef78e1b71711b5e036ceab7fcd486ba9
france14331 <59070042@it.kmitl.ac.th>
2019-02-17 10:33:12 +0700

    Scene_8

---
+++ week-5-unity-lab/Assets/_59070042/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Camera cameraTopView;
+    public Camera cameraFPSView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTopView = Camera.main;
+        cameraTopView.enabled = true;
+        cameraFPSView.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C)){
+            cameraTopView.enabled = !cameraTopView.enabled;
+            cameraFPSView.enabled = !cameraFPSView.enabled;
+        }
+    }
+}
