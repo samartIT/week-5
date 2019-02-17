5de7edbaefd84e2ccbd0af08f6bf75422151aed1
Munawang <59070036@it.kmitl.ac.th>
2019-02-17 22:44:30 +0700

    FPSInput

--- week-5-unity-lab/Assets/_59070036/Scripts/FPSInput.cs
+++ week-5-unity-lab/Assets/_59070036/Scripts/FPSInput.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class FPSInput : MonoBehaviour
 {
+    public float speed = 6.0f;
+    private CharacterController _characterController;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
     }
 }
