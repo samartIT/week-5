0c6d93f5d6ae545c0ec3fc08a93c614f0c2daaf7
it59070060 <bookinsapl2@gmail.com>
2019-02-17 14:07:02 +0700

    Scene8_MultipleCamera

---
+++ week-5-unity-lab/Assets/_59070060/Script/CamController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    Camera cameraTopView;
+    public Camera cameraFPSView;
+    void Start()
+    {
+        cameraTopView = Camera.main;
+        cameraTopView.enabled = true;
+        cameraFPSView.enabled = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            cameraTopView.enabled = !cameraTopView.enabled;
+            cameraFPSView.enabled = !cameraFPSView.enabled;
+        }
+    }
+}
