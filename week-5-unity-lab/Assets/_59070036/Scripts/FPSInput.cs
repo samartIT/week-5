dd435057bc901f14a6d64dbe5cc3fbd689e12f19
Munawang <59070036@it.kmitl.ac.th>
2019-02-17 22:49:30 +0700

    Add Player collision control

--- week-5-unity-lab/Assets/_59070036/Scripts/FPSInput.cs
+++ week-5-unity-lab/Assets/_59070036/Scripts/FPSInput.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FPSInput : MonoBehaviour
 {
     public float speed = 6.0f;
+    public float gravity = -9.8f;
     private CharacterController _characterController;
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,13 @@ public class FPSInput : MonoBehaviour
     {
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
-        transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _characterController.Move(movement);
+        
     }
 }
