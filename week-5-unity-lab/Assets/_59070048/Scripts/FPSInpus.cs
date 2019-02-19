9f485d56e651ad970839e7d05f9475976fb4f33f
Nattakit-Amonjarussang <59070048@kmitl.ac.th>
2019-02-19 21:49:39 +0700

    Part 9

---
+++ week-5-unity-lab/Assets/_59070048/Scripts/FPSInpus.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSInpus : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float gravity = -9.8f
+    private CharacterController _characterController;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horrizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _characterController.Move(movement);
+    }
+}
